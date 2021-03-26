d3a76fbb275ca445200a3923f63326ac78050bff
M Mustafa Türkben <mturkben721@gmail.com>
2021-03-26 22:52:26 +0300

    Delete index = 0.py

--- index = 0.py
+++
@@ -1,7 +0,0 @@
-    index = 0
-    for toTop in toTopIns:
-        if (index % 2 == 0):
-            print('name::',toTop.text)
-        else:
-            print('comment',toTop.text)
-        index = index + 1
